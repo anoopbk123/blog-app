0ad3abf2d7d0880b802817ddac76be9be120671d
Anoop B K <anoopkavalakkal@gmail.com>
2024-01-19 20:41:51 +0530

    theme and header design  comleted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,47 @@
 // Grab elements
-
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`${selector} is not found`);
+};
 //Nav styles on scroll
-
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }
+    else{
+        headerElement.classList.remove('activated');
+    }
+};
+window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
-
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem('currentTheme');
+if(currentTheme){
+    bodyElement.classList.toggle('light-theme')
+}
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
 
+    if(bodyElement.classList.contains('light-theme')){
+        localStorage.setItem('currentTheme', 'light-theme');
+    }
+    else{
+        localStorage.removeItem('currentTheme');
+    }
+})
 // Swiper
